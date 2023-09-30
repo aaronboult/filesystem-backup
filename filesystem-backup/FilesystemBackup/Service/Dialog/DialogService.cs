2a8b0949502f2ebbc3e7e8dc9cb3c0370cff1098
aaronboult25@gmail.com <aaronboult25@gmail.com>
2023-09-30 22:21:11 +0100

    Dialog service

---
+++ filesystem-backup/FilesystemBackup/Service/Dialog/DialogService.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace FilesystemBackup.Service.Dialog;
+
+public class DialogService : IDialogService
+{
+    public void ShowError(string message)
+    {
+        MessageBox.Show(message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+    }
+}
