cebd946e38e251db0551453d557245be6965f7b8
aaronboult25@gmail.com <aaronboult25@gmail.com>
2023-09-30 22:26:59 +0100

    Directory tree view model

---
+++ filesystem-backup/FilesystemBackup/ViewModel/DirectoryTree/IDirectoryTreeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using FilesystemBackup.Model;
+using FilesystemBackup.Service.Dialog;
+using System.Collections.ObjectModel;
+
+namespace FilesystemBackup.ViewModel.DirectoryTree
+{
+    public interface IDirectoryTreeViewModel
+    {
+        ObservableCollection<IDirectoryTreeViewModelItem> Items { get; }
+
+        void Rebuild(ScannedDirectory root, IProgressDialogService? dialog);
+    }
+}
