fbb97e4287b92c105c44a542ec046e458609b498
aaronboult25@gmail.com <aaronboult25@gmail.com>
2023-09-30 22:22:58 +0100

    ISerializable

---
+++ filesystem-backup/FilesystemBackup/Model/Serialized/ISerializable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FilesystemBackup.Model.Serialized;
+
+internal interface ISerializable
+{
+    int ByteLength { get; }
+    byte[] Serialize();
+    static ISerializable Deserialize(byte[] bytes)
+    {
+        throw new NotImplementedException($"Static Deserialize method not implemented for {nameof(ISerializable)}");
+    }
+}
