7d0d0d544c3dddcdf31371dbd3c723173f62d0db
aaronboult25@gmail.com <aaronboult25@gmail.com>
2023-09-30 22:27:14 +0100

    Load view models into container

---
+++ filesystem-backup/FilesystemBackup/ViewModel/Bootstrapper.cs
@@ -0,0 +1,14 @@
+﻿using FilesystemBackup.ViewModel.DirectoryTree;
+using FilesystemBackup.ViewModel.MainWindow;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FilesystemBackup.ViewModel;
+
+public static class Bootstrapper
+{
+    public static void AddViewModels(this IServiceCollection services)
+    {
+        services.AddSingleton<MainWindowViewModel>();
+        services.AddSingleton<IDirectoryTreeViewModel, DirectoryTreeViewModel>();
+    }
+}
