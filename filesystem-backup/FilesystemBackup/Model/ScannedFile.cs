a1c7fc9f91207933169efe80ad4d59fa67363b72
aaronboult25@gmail.com <aaronboult25@gmail.com>
2023-09-30 22:22:41 +0100

    Scanned file model

---
+++ filesystem-backup/FilesystemBackup/Model/ScannedFile.cs
@@ -0,0 +1,19 @@
+﻿namespace FilesystemBackup.Model;
+
+public readonly struct ScannedFile
+{
+    public readonly string Path { get; }
+    public readonly string Name => System.IO.Path.GetFileName(Path);
+
+
+    public ScannedFile(string path)
+    {
+        Path = path;
+    }
+
+
+    public byte[] Serialize()
+    {
+        return System.Text.Encoding.UTF8.GetBytes(Path);
+    }
+}
